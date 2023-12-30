53203c0eefe5409aeb444eeb0faca7142a222266
PrajjavalSrivastava <83862069+PrajjavalSrivastava@users.noreply.github.com>
2023-12-30 17:50:33 +0530

    Add files via upload

---
+++ 30-12-2023/01.js
@@ -0,0 +1,7 @@
+function HouseKeeper (yearsOfExperience, name, cleaningRepertoire) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+}
+
+var houseKeeper1 = new HouseKeeper1(9, "Tom", ["lobby", "bedroom"]);
